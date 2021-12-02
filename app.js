390dfc8392c26f321aec35c04570b47fb115827d
johnzz <zhuangzhong23@gmail.com>
2021-12-02 21:57:06 +1100

    updated

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 let clickTime = 0;
 let cells = document.querySelectorAll('.cell');
 cells.forEach(function (cell) {
-    cell.addEventListener('click', click)
-
+    cell.addEventListener('click', click, { once: true })
 })
 
 function click(event) {
-    event.target.classList.add('clicked-cell-x');
-    if (event.target.classList.contains('clicked-cell-x') == false) {
+    if (clickTime % 2 == 0 && event.target.classList.contains('clicked-cell-o') == false) {
+        event.target.classList.add('clicked-cell-x');
+    } else if (event.target.classList.contains('clicked-cell-x') == false && clickTime % 2 !== 0) {
         event.target.classList.add('clicked-cell-o');
     }
-
-
-
-    // if (clickTime % 2 == 0 && event.target.classList.contains('clicked-cell-o') == false) {
-    //     event.target.classList.add('clicked-cell-x');
-    // } else if (event.target.classList.contains('clicked-cell-x') == false && clickTime % 2 !== 0) {
-    //     event.target.classList.add('clicked-cell-o');
-    // }
-    // clickTime++;
-
+    clickTime++;
 }
+
+
