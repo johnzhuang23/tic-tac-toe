e42c65627edd957267c391f3b78cf4a3c0b5ee32
johnzz <zhuangzhong23@gmail.com>
2021-12-03 23:46:57 +1100

    js completed

--- app.js
+++ app.js
@@ -1,117 +1,76 @@
 // debugger;
+let cell1 = document.getElementById('one');
+let cell2 = document.getElementById('two');
+let cell3 = document.getElementById('three');
+let cell4 = document.getElementById('four');
+let cell5 = document.getElementById('five');
+let cell6 = document.getElementById('six');
+let cell7 = document.getElementById('seven');
+let cell8 = document.getElementById('eight');
+let cell9 = document.getElementById('nine');
 
 let cells = document.querySelectorAll('section div');
-let resetBtn = document.querySelector('.reset-button');
-resetBtn.addEventListener('click', resetButton);
-function resetButton() {
-    for (let i = 0; i < 9; i++) {
-        if (cells[i].classList.contains('clicked-cell-x')) {
-            cells[i].classList.remove('clicked-cell-x');
-        } else {
-            cells[i].classList.remove('clicked-cell-o');
-        }
-        cells[i].addEventListener('click', click, { once: true })
-    }
-    document.querySelector('.message').textContent = '';
-    clickTime = 0;
-    hasWinner = false;
-};
 
-let clickTime = 0;
-let hasWinner = false;
+function newDataNumber(cellName) {
+    return Number(cellName.dataset.number)
+}
 
 cells.forEach(function (cell) {
     cell.addEventListener('click', click, { once: true })
 })
+
+let clickTime = 0;
+let winner = false;
 function click(event) {
-    if (hasWinner == true) {
-        return;
-    }
-    // if (document.querySelector('.message').textContent !== '') {
+    // if(winner = true){
     //     return;
     // }
-    if (clickTime % 2 == 0) {
-        event.target.classList.toggle('clicked-cell-x');
-    } else {
-        event.target.classList.toggle('clicked-cell-o');
+    if (document.querySelector('.message').textContent !== '') {
+        return;
     }
-    if ((document.querySelector('.one').classList.contains('clicked-cell-x') == true &&
-        document.querySelector('.two').classList.contains('clicked-cell-x') == true &&
-        document.querySelector('.three').classList.contains('clicked-cell-x') == true)
-        ||
-        (document.querySelector('.four').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.five').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.six').classList.contains('clicked-cell-x') == true)
-        ||
-        (document.querySelector('.seven').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.eight').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.nine').classList.contains('clicked-cell-x') == true)
-        ||
-        (document.querySelector('.one').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.four').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.seven').classList.contains('clicked-cell-x') == true)
-        ||
-        (document.querySelector('.two').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.five').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.eight').classList.contains('clicked-cell-x') == true)
-        ||
-        (document.querySelector('.three').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.six').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.nine').classList.contains('clicked-cell-x') == true)
-        ||
-        (document.querySelector('.one').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.five').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.nine').classList.contains('clicked-cell-x') == true)
-        ||
-        (document.querySelector('.three').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.five').classList.contains('clicked-cell-x') == true &&
-            document.querySelector('.seven').classList.contains('clicked-cell-x') == true)
+    if (clickTime % 2 == 0) {
+        event.target.dataset.number = 1;
+    } else {
+        event.target.dataset.number = -1;
+    }
+    if (newDataNumber(cell1) + newDataNumber(cell2) + newDataNumber(cell3) == 3 ||
+        newDataNumber(cell1) + newDataNumber(cell4) + newDataNumber(cell7) == 3 ||
+        newDataNumber(cell1) + newDataNumber(cell5) + newDataNumber(cell9) == 3 ||
+        newDataNumber(cell2) + newDataNumber(cell5) + newDataNumber(cell8) == 3 ||
+        newDataNumber(cell3) + newDataNumber(cell5) + newDataNumber(cell7) == 3 ||
+        newDataNumber(cell3) + newDataNumber(cell6) + newDataNumber(cell9) == 3 ||
+        newDataNumber(cell4) + newDataNumber(cell5) + newDataNumber(cell6) == 3 ||
+        newDataNumber(cell7) + newDataNumber(cell8) + newDataNumber(cell9) == 3
     ) {
         document.querySelector('.message').textContent = 'The winner is X! Click Restart to play again!';
-        hasWinner = true;
-
+        // winner = true;
     } else if (
-        (document.querySelector('.one').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.two').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.three').classList.contains('clicked-cell-o') == true)
-        ||
-        (document.querySelector('.four').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.five').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.six').classList.contains('clicked-cell-o') == true)
-        ||
-        (document.querySelector('.seven').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.eight').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.nine').classList.contains('clicked-cell-o') == true)
-        ||
-        (document.querySelector('.one').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.four').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.seven').classList.contains('clicked-cell-o') == true)
-        ||
-        (document.querySelector('.two').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.five').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.eight').classList.contains('clicked-cell-o') == true)
-        ||
-        (document.querySelector('.three').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.six').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.nine').classList.contains('clicked-cell-o') == true)
-        ||
-        (document.querySelector('.one').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.five').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.nine').classList.contains('clicked-cell-o') == true)
-        ||
-        (document.querySelector('.three').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.five').classList.contains('clicked-cell-o') == true &&
-            document.querySelector('.seven').classList.contains('clicked-cell-o') == true)
+        newDataNumber(cell1) + newDataNumber(cell2) + newDataNumber(cell3) == -3 ||
+        newDataNumber(cell1) + newDataNumber(cell4) + newDataNumber(cell7) == -3 ||
+        newDataNumber(cell1) + newDataNumber(cell5) + newDataNumber(cell9) == -3 ||
+        newDataNumber(cell2) + newDataNumber(cell5) + newDataNumber(cell8) == -3 ||
+        newDataNumber(cell3) + newDataNumber(cell5) + newDataNumber(cell7) == -3 ||
+        newDataNumber(cell3) + newDataNumber(cell6) + newDataNumber(cell9) == -3 ||
+        newDataNumber(cell4) + newDataNumber(cell5) + newDataNumber(cell6) == -3 ||
+        newDataNumber(cell7) + newDataNumber(cell8) + newDataNumber(cell9) == -3
     ) {
-        document.querySelector('.message').textContent = 'The winner is O! Click Restart to play again!'
-        hasWinner = true;
+        document.querySelector('.message').textContent = 'The winner is O! Click Restart to play again!';
+        // winner = true;
+    } else if (clickTime >= 8 && document.querySelector('.message').textContent == '') {
+        document.querySelector('.message').textContent = 'draw';
+        // winner = true;
     }
     clickTime++;
 }
 
-
-// each cell has a unique ID, then let odd times of click assign a cell a number of "1", represening "X", 
-// the even number times of click assign the cell "-1", representing "O",
-// the winning conditions are to see from the 5th step, among the the combination of cells 123/456/789/147/258/369/159/357 , which one's sum reaches 3 or -3 first. 3 means x win, -3 means O wins.
-
-
+let resetBtn = document.querySelector('.reset-button');
+resetBtn.addEventListener('click', resetButton);
+function resetButton() {
+    for (let i = 0; i < 9; i++) {
+        cells[i].dataset.number = 0;
+        cells[i].addEventListener('click', click, { once: true })
+    }
+    document.querySelector('.message').textContent = '';
+    clickTime = 0;
+    winner = false;
+};
